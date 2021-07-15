20fed6334d36fb83dd77a596308971170f5c8bf0
Dan920-Dev <escobare361@gmail.com>
2021-07-15 16:50:26 -0600

    cine app

---
+++ cineAppNCapas/Controllers/ACCESOPREMIERsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cineAppNCapas.Models;
+
+namespace cineAppNCapas.Controllers
+{
+    public class ACCESOPREMIERsController : Controller
+    {
+        private dbCineNCapasaEntities db = new dbCineNCapasaEntities();
+
+        // GET: ACCESOPREMIERs
+        public ActionResult Index()
+        {
+            var aCCESOPREMIER = db.ACCESOPREMIER.Include(a => a.PERSONA).Include(a => a.PELICULA);
+            return View(aCCESOPREMIER.ToList());
+        }
+
+        // GET: ACCESOPREMIERs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACCESOPREMIER aCCESOPREMIER = db.ACCESOPREMIER.Find(id);
+            if (aCCESOPREMIER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCCESOPREMIER);
+        }
+
+        // GET: ACCESOPREMIERs/Create
+        public ActionResult Create()
+        {
+            ViewBag.idPersona = new SelectList(db.PERSONA, "idPersona", "nombre");
+            ViewBag.nombrePelicula = new SelectList(db.PELICULA, "nombre", "clasificacion");
+            return View();
+        }
+
+        // POST: ACCESOPREMIERs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idPremier,nombrePelicula,idPersona")] ACCESOPREMIER aCCESOPREMIER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ACCESOPREMIER.Add(aCCESOPREMIER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idPersona = new SelectList(db.PERSONA, "idPersona", "nombre", aCCESOPREMIER.idPersona);
+            ViewBag.nombrePelicula = new SelectList(db.PELICULA, "nombre", "clasificacion", aCCESOPREMIER.nombrePelicula);
+            return View(aCCESOPREMIER);
+        }
+
+        // GET: ACCESOPREMIERs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACCESOPREMIER aCCESOPREMIER = db.ACCESOPREMIER.Find(id);
+            if (aCCESOPREMIER == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idPersona = new SelectList(db.PERSONA, "idPersona", "nombre", aCCESOPREMIER.idPersona);
+            ViewBag.nombrePelicula = new SelectList(db.PELICULA, "nombre", "clasificacion", aCCESOPREMIER.nombrePelicula);
+            return View(aCCESOPREMIER);
+        }
+
+        // POST: ACCESOPREMIERs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idPremier,nombrePelicula,idPersona")] ACCESOPREMIER aCCESOPREMIER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aCCESOPREMIER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idPersona = new SelectList(db.PERSONA, "idPersona", "nombre", aCCESOPREMIER.idPersona);
+            ViewBag.nombrePelicula = new SelectList(db.PELICULA, "nombre", "clasificacion", aCCESOPREMIER.nombrePelicula);
+            return View(aCCESOPREMIER);
+        }
+
+        // GET: ACCESOPREMIERs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACCESOPREMIER aCCESOPREMIER = db.ACCESOPREMIER.Find(id);
+            if (aCCESOPREMIER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCCESOPREMIER);
+        }
+
+        // POST: ACCESOPREMIERs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ACCESOPREMIER aCCESOPREMIER = db.ACCESOPREMIER.Find(id);
+            db.ACCESOPREMIER.Remove(aCCESOPREMIER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ cineAppNCapas/Controllers/PELICULAsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cineAppNCapas.Models;
+
+namespace cineAppNCapas.Controllers
+{
+    public class PELICULAsController : Controller
+    {
+        private dbCineNCapasaEntities db = new dbCineNCapasaEntities();
+
+        // GET: PELICULAs
+        public ActionResult Index()
+        {
+            return View(db.PELICULA.ToList());
+        }
+
+        // GET: PELICULAs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PELICULA pELICULA = db.PELICULA.Find(id);
+            if (pELICULA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pELICULA);
+        }
+
+        // GET: PELICULAs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PELICULAs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombre,fecha,duracion,clasificacion,estudio,director,genero")] PELICULA pELICULA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PELICULA.Add(pELICULA);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pELICULA);
+        }
+
+        // GET: PELICULAs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PELICULA pELICULA = db.PELICULA.Find(id);
+            if (pELICULA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pELICULA);
+        }
+
+        // POST: PELICULAs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "nombre,fecha,duracion,clasificacion,estudio,director,genero")] PELICULA pELICULA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pELICULA).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pELICULA);
+        }
+
+        // GET: PELICULAs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PELICULA pELICULA = db.PELICULA.Find(id);
+            if (pELICULA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pELICULA);
+        }
+
+        // POST: PELICULAs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            PELICULA pELICULA = db.PELICULA.Find(id);
+            db.PELICULA.Remove(pELICULA);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
